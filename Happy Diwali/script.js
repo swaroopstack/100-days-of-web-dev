ff5f636d6a1119dc8c760ed96931f1d97eb2f71a
swaroopstack <swarooppokemon@gmail.com>
2025-11-04 21:01:48 +0530

    Completed rebase and organized all folders properly

--- Happy Diwali/script.js
+++ Happy Diwali/script.js
@@ -5,36 +5,34 @@ const fireworks = document.getElementById("fireworks");
 const confetti = document.getElementById("confetti");
 
 box.addEventListener("click", () => {
-    box.classList.add("open");
+  box.classList.add("open");
 
-    setTimeout(() => {
-        rocket.style.display = "block";
-        rocket.classList.add("launch");
-    }, 800);
+  setTimeout(() => {
+    rocket.style.display = "block";
+    rocket.classList.add("launch");
+  }, 800);
 });
 
 rocket.addEventListener("animationend", () => {
-    text.style.display = "block";
-    text.classList.add("show");
+  text.style.display = "block";
+  text.classList.add("show");
 
-    // Fireworks
-    fireworks.style.display = "block";
-    for (let i = 0; i < 20; i++) {
-        const span = document.createElement("span");
-        span.style.setProperty('--x', (Math.random() * 200 - 100));
-        span.style.setProperty('--y', (Math.random() * 200 - 100));
-        fireworks.appendChild(span);
-        setTimeout(() => { span.remove(); }, 1000);
-    }
+  fireworks.style.display = "block";
+  for (let i = 0; i < 20; i++) {
+    const span = document.createElement("span");
+    span.style.setProperty('--x', (Math.random() * 200 - 100));
+    span.style.setProperty('--y', (Math.random() * 200 - 100));
+    fireworks.appendChild(span);
+    setTimeout(() => { span.remove(); }, 1000);
+  }
 
-    // Confetti
-    confetti.style.display = "block";
-    for (let i = 0; i < 30; i++) {
-        const span = document.createElement("span");
-        span.style.setProperty('--x', (Math.random() * 200 - 100) + 'px');
-        span.style.setProperty('--y', (Math.random() * 200 + 100) + 'px');
-        span.style.setProperty('--hue', Math.floor(Math.random() * 360));
-        confetti.appendChild(span);
-        setTimeout(() => { span.remove(); }, 2000);
-    }
+  confetti.style.display = "block";
+  for (let i = 0; i < 30; i++) {
+    const span = document.createElement("span");
+    span.style.setProperty('--x', (Math.random() * 200 - 100) + 'px');
+    span.style.setProperty('--y', (Math.random() * 200 + 100) + 'px');
+    span.style.setProperty('--hue', Math.floor(Math.random() * 360));
+    confetti.appendChild(span);
+    setTimeout(() => { span.remove(); }, 2000);
+  }
 });

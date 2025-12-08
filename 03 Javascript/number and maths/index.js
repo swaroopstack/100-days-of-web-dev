38e4b22d85ff6af98d5d1da0fc49b59f0a8233f9
swaroopstack <swarooppokemon@gmail.com>
2025-12-08 11:46:58 +0530

    numbers in js

---
+++ 03 Javascript/number and maths/index.js
@@ -0,0 +1,6 @@
+let a=20;
+let b=3.14654
+console.log(b.toFixed(2));  //to print the number after the decimal
+//it will not change the real value of b
+console.log(typeof b.toFixed(2));  // type is string
+console.log(b.toPrecision(5)); // kitni value leni hai

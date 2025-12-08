59dd013ef20110fa82de6a33b23ac0afbd0ef2de
swaroopstack <swarooppokemon@gmail.com>
2025-12-08 13:09:41 +0530

    new method to declare number

--- 03 Javascript/number and maths/index.js
+++ 03 Javascript/number and maths/index.js
@@ -4,3 +4,10 @@ console.log(b.toFixed(2));  //to print the number after the decimal
 //it will not change the real value of b
 console.log(typeof b.toFixed(2));  // type is string
 console.log(b.toPrecision(5)); // kitni value leni hai
+console.log(b.toString())
+
+//new method to declare number
+let c= new Number(20);  //type is object
+let d=30;               //type is number
+
+console.log(c==d);    // false

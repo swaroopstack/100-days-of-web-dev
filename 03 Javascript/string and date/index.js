ae6347e1864e913b4cd2449b0e7a43dabea4d7bb
swaroopstack <swarooppokemon@gmail.com>
2025-12-09 14:59:55 +0530

    date and time notes

--- 03 Javascript/string and date/index.js
+++ 03 Javascript/string and date/index.js
@@ -36,4 +36,36 @@ console.log(names.split(",")) //gives an array
 console.log(10+"HELLo"+20);  // 10HELLO20
 console.log(10+20+"HELLo");  // 30HELLO
 
+// DATE-Time
 
+const now= new Date(); // give UTC date
+console.log(now.toString()); //give correct and proper formatted date
+console.log(now.toISOString());
+console.log(now.toLocaleString());
+
+//below operations give date from local time
+console.log(now.getDay()); //gives day in number
+console.log(now.getDate());
+console.log(now.getMonth()); //IMP- months are taken from 0 onwards (Jan is 0)
+
+//want to create own date
+// year month date hour minute second millisecond 
+const owndate= new Date(2025,11,12,11,45,34,127);
+//but there is inconsistency in output the month will be diff
+// this inconsistency came from java later java removed this inconsistency 
+// why this inconsistenecy is not removed in js?
+// ans- because many website are made on this inconsistency if changed , all the website will suffer
+
+const current = Date.current();  // output is in milliseconds
+//if we put this output in date(here) it will give exact date
+
+//const old= Date.old();    ---> Thu Jan 01 1970
+
+//How website that host contest manage two diff time zone when checking of a winner
+// eg- if someone from india started contest at 4:51 am, 1 oct
+// USA guy started the contest from 7:21pm, 30 sept 
+//what if indian took 10 minutes to solve the problem while usa student took 20 min 
+// then the time of the usa student is still ahead because of time diff
+// ans- we will check univesal time UTC
+
+//UTC importance?

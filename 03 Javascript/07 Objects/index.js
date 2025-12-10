509e9243fe7a57355b91c86baa0c4ec80ab312dd
swaroopstack <swarooppokemon@gmail.com>
2025-12-10 16:34:04 +0530

    minor updates

--- 03 Javascript/07 Objects/index.js
+++ 03 Javascript/07 Objects/index.js
@@ -23,11 +23,56 @@ console.log(Object.keys(user));
 console.log(Object.values(user));
 console.log(Object.entries(user));
 
+// not recommended to use loop in this way
 for(let keys in user){
     console.log(keys,user[keys]);
     // console.log(keys,user.keys);  gives undifined, why?
 }
 
+//destructuring of object
 const {name,age}=user;
-console.log(name,user);
+//const { name: username,age: userage }=user;
+console.log(name,age);
+
+//destructuring of array
+const arr=[12,13,14,14,56]
+const {first,second}=arr;
+console.log(first,second);
+
+// for loop of array 
+const temparr=Object.keys(user);
+for(let keys of temparr){
+    console.log(keys);
+}
+
+//this will not give you output in array
+for(let [keys,values] of Object.entries(user)){
+    console.log(keys,values);
+}
+
+//function in objects
+const user={
+    name:"Swaroop",
+    age:19,
+    greeting: function(){
+        console.log(`hellooo mitrooo ${this.name} `);  // what is "this" , why we use it
+    }
+}
+
+//what if we dont use this and instead use this ${user.name} in greeting above
+const user2={
+    name:"dev",
+    age:20
+}
+
+user2.greeting=user.greeting;
+user2.greeting();   //we will get greetings from user and the name will be displayed will be from user
+
+// nested objects? 
+
+//shallow copy
+//const user2={...user};   why we do this ?? why it oly handle till one level
+
+//const user2=structuredClone(user);      it handle nested also
+// whats its limitations?
 
